df36967640a9e297b335e1301e5f5e838c8f52f7
SayPole <161555593+SayPole@users.noreply.github.com>
2026-05-25 13:21:01 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-   // Cek user aktif dulu sebelum attempt
-$user = \App\Models\User::where('email', $credentials['email'])->first();
 
 class AuthController extends Controller
 {
